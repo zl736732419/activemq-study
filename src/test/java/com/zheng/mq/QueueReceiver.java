58be817d4c12fd8359d4060ecb8337d7e9ff4572
zhenglian <736732419@qq.com>
2018-04-17 15:28:51 +0800

    修改activemq代码编写结构

---
+++ src/test/java/com/zheng/mq/QueueReceiver.java
@@ -0,0 +1,108 @@
+package com.zheng.mq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import java.util.Enumeration;
+import java.util.Optional;
+
+/**
+ * @Author zhenglian
+ * @Date 2018/4/17 15:22
+ */
+public class QueueReceiver {
+    private static final String BROKER_URL = "tcp://zl52:61616";
+    private static final String QUEUE = "test_queue";
+
+    private Connection connection;
+    private Session session;
+    private MessageConsumer consumer;
+    
+    private boolean printJMSProperty = false;
+
+    @Before
+    public void init() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
+        try {
+            connection = factory.createConnection();
+            connection.start();
+
+            printJMSPropertyName();
+
+            session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue(QUEUE);
+            consumer = session.createConsumer(destination);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printJMSPropertyName() throws JMSException {
+        if (!printJMSProperty) {
+            return;
+        }
+        Enumeration<String> propertyNames = connection.getMetaData().getJMSXPropertyNames();
+        String propertyName;
+        while (propertyNames.hasMoreElements()) {
+            propertyName = propertyNames.nextElement();
+            System.out.println(propertyName);
+        }
+    }
+
+    @Test
+    public void receiveMapMessage() throws JMSException {
+        MapMessage mapMessage;
+        String msg;
+        String property;
+        for (int i = 0; i < 3; i++) {
+            mapMessage = (MapMessage) consumer.receive();
+            msg = mapMessage.getString("message---" + i);
+            System.out.println("received msg: " + msg);
+            property = mapMessage.getStringProperty("extra");
+            System.out.println("received Property: " + property);
+        }
+    }
+
+    @Test
+    public void receiveTextMessage() throws JMSException {
+        TextMessage textMessage;
+        String text = null;
+        for (int i = 0; i < 3; i++) {
+            textMessage = (TextMessage) consumer.receive();
+            try {
+                text = textMessage.getText();
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Received: " + text);
+        }
+    }
+
+    @After
+    public void cleanup() {
+        try {
+            session.commit();
+            if (Optional.ofNullable(consumer).isPresent()) {
+                consumer.close();
+            }
+            if (Optional.ofNullable(session).isPresent()) {
+                session.close();
+            }
+            if (Optional.ofNullable(connection).isPresent()) {
+                connection.close();
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        System.out.println("消息处理完毕!");
+    }
+}
