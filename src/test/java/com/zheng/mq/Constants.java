1808d13d43de1cd7eb4d9d4803a1061ab4a6c645
z736732419 <736732419@qq.com>
2018-04-18 23:39:05 +0800

    activemq消息持久化方式学习

--- src/test/java/com/zheng/mq/Constants.java
+++ src/test/java/com/zheng/mq/Constants.java
@@ -5,13 +5,13 @@ package com.zheng.mq;
  * @Date 2018/4/17 20:38
  */
 public class Constants {
-    public static final String BROKER_URL = "tcp://localhost:61616";
+//    public static final String BROKER_URL = "tcp://localhost:61616";
 //    public static final String BROKER_URL = "vm://localhost";
 //    public static final String BROKER_URL = "http://zl52:61621";
 //    public static final String BROKER_URL = "ssl://zl52:61619";
 //    public static final String BROKER_URL = "udp://zl52:61620";
 //    public static final String BROKER_URL = "nio://zl52:61618";
-//    public static final String BROKER_URL = "tcp://zl52:61616";
+    public static final String BROKER_URL = "tcp://zl202:61616";
     public static final String QUEUE = "ssl_queue";
     public static final String TOPIC = "my_topic";
 
