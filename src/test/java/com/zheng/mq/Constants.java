fd4f6b6ae61fd01d94f3de97b5e24ffd3703a520
zhenglian <736732419@qq.com>
2018-04-18 15:31:46 +0800

    添加udp协议

--- src/test/java/com/zheng/mq/Constants.java
+++ src/test/java/com/zheng/mq/Constants.java
@@ -5,7 +5,8 @@ package com.zheng.mq;
  * @Date 2018/4/17 20:38
  */
 public class Constants {
-    public static final String BROKER_URL = "nio://zl52:61618";
+    public static final String BROKER_URL = "udp://zl52:61620";
+//    public static final String BROKER_URL = "nio://zl52:61618";
 //    public static final String BROKER_URL = "tcp://zl52:61616";
     public static final String QUEUE = "test_queue";
     public static final String TOPIC = "my_topic";
