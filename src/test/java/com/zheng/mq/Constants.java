cf826075b7a99d987a34b705e6d213d8b0e18847
z736732419 <736732419@qq.com>
2018-04-18 22:11:47 +0800

    实现本地启动broker并设置kahadb参数

--- src/test/java/com/zheng/mq/Constants.java
+++ src/test/java/com/zheng/mq/Constants.java
@@ -5,7 +5,8 @@ package com.zheng.mq;
  * @Date 2018/4/17 20:38
  */
 public class Constants {
-    public static final String BROKER_URL = "vm://localhost";
+    public static final String BROKER_URL = "tcp://localhost:61616";
+//    public static final String BROKER_URL = "vm://localhost";
 //    public static final String BROKER_URL = "http://zl52:61621";
 //    public static final String BROKER_URL = "ssl://zl52:61619";
 //    public static final String BROKER_URL = "udp://zl52:61620";
@@ -14,8 +15,8 @@ public class Constants {
     public static final String QUEUE = "ssl_queue";
     public static final String TOPIC = "my_topic";
 
-    public static String KEY_STORE = "C:\\Users\\Administrator\\Desktop\\client1.ks";
-    public static String TRUST_STORE = "C:\\Users\\Administrator\\Desktop\\client1.ts";
+    public static String KEY_STORE = "client1.ks";
+    public static String TRUST_STORE = "client1.ts";
     public static String KEY_STORE_PASSWORD = "123456";
     
 }
--- src/test/java/com/zheng/mq/broker/BrokerServiceApp.java
+++ src/test/java/com/zheng/mq/broker/BrokerServiceApp.java
@@ -1,6 +1,9 @@
 package com.zheng.mq.broker;
 
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.store.kahadb.KahaDBStore;
+
+import java.io.File;
 
 /**
  * 通过brokerService启动mq实例
@@ -11,7 +14,20 @@ import org.apache.activemq.broker.BrokerService;
 public class BrokerServiceApp {
     
     public static void main(String[] args) throws Exception {
+        // 设置kahadb相关参数
+        // 设置kahadb log file目录
+        File dataFileDir = new File("target/activemq/kahadb");
+        KahaDBStore store = new KahaDBStore();
+        store.setDirectory(dataFileDir);
+        // 设置log data file最大100MB
+        store.setJournalMaxFileLength(1024 * 1024 * 100);
+        store.setEnableIndexWriteAsync(true);
+
+
         BrokerService brokerService = new BrokerService();
+        // 使用kahadb存储数据
+        brokerService.setPersistenceAdapter(store);
+
         brokerService.setUseJmx(true);
         brokerService.addConnector("tcp://localhost:61616");
         brokerService.start();
