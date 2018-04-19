6d0a08dc04e4085f1f7c171a9868199ce30b8f9a
zhenglian <736732419@qq.com>
2018-04-19 16:17:49 +0800

    添加容错链接

--- src/test/java/com/zheng/mq/Constants.java
+++ src/test/java/com/zheng/mq/Constants.java
@@ -11,7 +11,8 @@ public class Constants {
 //    public static final String BROKER_URL = "ssl://zl52:61619";
 //    public static final String BROKER_URL = "udp://zl52:61620";
 //    public static final String BROKER_URL = "nio://zl52:61618";
-    public static final String BROKER_URL = "tcp://zl52:61616";
+//    public static final String BROKER_URL = "tcp://zl52:61616";
+    public static final String BROKER_URL = "failover:(tcp://zl52:61616,tcp://zl52:61716)?randomize=true";
     public static final String QUEUE = "test_queue";
     public static final String TOPIC = "my_topic";
 
