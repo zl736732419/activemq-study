4209c1923879c587841e4a7ffe0c5611dd420427
zhenglian <736732419@qq.com>
2018-04-23 16:53:19 +0800

    添加独占消费者，消息分组测试

---
+++ src/test/java/com/zheng/mq/consumer/group/QueueReceiver.java
@@ -0,0 +1,50 @@
+package com.zheng.mq.consumer.group;
+
+import com.zheng.mq.Constants;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+/**
+ * @Author zhenglian
+ * @Date 2018/4/17 15:22
+ */
+public class QueueReceiver {
+    private static final String BROKER_URL = Constants.BROKER_URL;
+    private static final String QUEUE = Constants.QUEUE;
+
+    public static void main(String[] args) {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
+        try {
+            Connection connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue(QUEUE);
+            for(int i = 0; i < 2; i++) {
+                MessageConsumer consumer = session.createConsumer(destination);
+                consumer.setMessageListener(new MessageListener() {
+                    @Override
+                    public void onMessage(Message message) {
+                        TextMessage textMessage = (TextMessage) message;
+                        try {
+                            System.out.println(consumer + " received: " + textMessage.getText());
+                            session.commit();
+                        } catch (JMSException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
